0fbec414812fed4a8fb407b29ee58852098f4ce2
Yooni <yooni.kj.yoon@gmail.com>
2023-01-28 11:51:33 +0900

    set react router and react query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Main from "./pages/Main";
+import NotFound from "./pages/NotFound";
+import Videos from "./pages/Videos";
+import VideoDetail from "./pages/VideoDetail";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: "/videos/:videoId", element: <VideoDetail /> },
+    ],
+  },
+]);
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
   );
 }
 
