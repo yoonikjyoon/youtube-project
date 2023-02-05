e67185033d3d02038c384d35b8e4567f59d01088
Yooni <yooni.kj.yoon@gmail.com>
2023-02-05 16:34:34 +0900

    use real data

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useContext } from "react";
-import FakeYoutubeClient from "../api/fakeYoutubeClient";
 import Youtube from "../api/youtube";
-// import YoutubeClient from "../api/youtubeClient";
+import YoutubeClient from "../api/youtubeClient";
+// import FakeYoutubeClient from "../api/fakeYoutubeClient";
 
 export const YoutubeApiContext = createContext();
 
-// const client = new YoutubeClient();
-const client = new FakeYoutubeClient();
+const client = new YoutubeClient();
+// const client = new FakeYoutubeClient();
 const youtube = new Youtube(client);
 
 export function YoutubeApiProvider({ children }) {
